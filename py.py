71b7a9cfbf9ee1ae4de57e776dc80b63f30f78b2
JCamyre <JWcamry03@gmail.com>
2021-06-15 22:53:14 -0700

    Working on HashTable.

--- py.py
+++ py.py
@@ -1,11 +1,26 @@
-class HashTable():
+class HashTable():  
     def __init__(self):
-        pass    
+        self.arr = []
+    
+    def append(self, key, val):
+        key = hash(key)
+        i = key % len(self.arr)
+        node = SingleLinkedNode(val=val)
+        # might have issue with this line, since we Python array are dynamically changing in size, there won't be any spaces available, which means there will never be an empty element
+        # could do self.arr = [None] * 20
+        if self.arr[i]:
+            cur_node = self.arr[i]
+            while cur_node.next:
+                cur_node = cur_node.next
+            cur_node.next = node               
         
 class SingleLinkedNode():
     def __init__(self, val=None):
         self.val = val
         self.next = None
+    
+# different hash value every time
+print(hash('joseph'))
         
 # 1.1: Most obvious would be to use hash table. How much more efficient is hash table than dictionary?
 # Just loop through string, map each letter to hash map/dictionary. If letter already in hashmap, return False.
@@ -31,4 +46,5 @@ def is_unique_bruteforce(string):
     return True
 
 # Best conceivable runtime (BCR) is O(N), since you have to loop through all elements atleast once.
+
 
